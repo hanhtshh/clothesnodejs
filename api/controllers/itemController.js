c7071d5ee185bde4422695d46d198ab698252bee
Nguyen Van Hanh <nguyenhanh3@telio.vn>
2023-05-12 09:39:39 +0700

    ofk

--- api/controllers/itemController.js
+++ api/controllers/itemController.js
@@ -7,7 +7,7 @@ class ItemController {
             const keySearch = req.query.keySearch;
             if (keySearch) {
                 const list = await itemModel.find({
-                    "name": { "$regex": keySearch }
+                    "name": new RegExp('^' + keySearch + '$', "i")
                 })
                     .populate("category");
                 res.json(list);
