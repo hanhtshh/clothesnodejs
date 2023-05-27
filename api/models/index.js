a419a8f376fc8e6429a0d9b86c926286d3c2095d
unknown <hanh1452001@gmail.com>
2023-05-28 05:15:35 +0700

    change db

--- api/models/index.js
+++ api/models/index.js
@@ -2,8 +2,9 @@ const mongoose=require('mongoose');
 
 module.exports=async function(){
     try{
-        const db=await mongoose.connect('mongodb+srv://hanhtshh:mhsdm6dk@clothes.3gvxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+        const db=await mongoose.connect('mongodb+srv://hanhtshh:mhsdm6dk@cluster0.7s3ios8.mongodb.net/');
         console.log('Ket noi den database thanh cong');
+
     }
     catch(err){
         console.log('Ket noi den database that bai');
